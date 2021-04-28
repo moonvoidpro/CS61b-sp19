b58eae74e97f1185eb66fb0a29b9d3b19b0e1f8f
moonvoid <29794262+moonvoidpro@users.noreply.github.com>
2021-04-28 09:51:36 +0800

    Project 0

--- proj0/Body.java
+++ proj0/Body.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Body {
+public class Body {
+    public Body(double xP, double yP, double xV, double yV, double m, String img) 
+    public double xxPos() {}
+    public double yyPos() {}
+    public double xxVel() {}
+    public double yyVel() {}
+    public double mass() {}
+    public String imgFileName() {}
+
+    
 }
--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class NBody {
+public class NBody {
 }
