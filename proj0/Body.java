563fe4bb86d67ef7799e9c7478d9d443f6384471
moonvoid <29794262+moonvoidpro@users.noreply.github.com>
2021-04-29 23:20:07 +0800

    finish Body.java

--- proj0/Body.java
+++ proj0/Body.java
@@ -24,4 +24,55 @@ public class Body {
         this.mass = b.mass;
         this.imgFileName = b.imgFileName;
     }
+    public double calcDistance(Body b)
+    {
+        double xDistance = this.xxPos - b.xxPos;
+        double yDistance = this.yyPos - b.yyPos;
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    }
+    public double calcForceExertedBy(Body b)
+    {
+        double constantG = 6.67E-11;
+        return (constantG * this.mass * b.mass)/(this.calcDistance(b) * this.calcDistance(b));
+    }
+    public double calcForceExertedByX(Body b)
+    {
+        double xGap = b.xxPos - this.xxPos;
+        return (this.calcForceExertedBy(b) * xGap)/this.calcDistance(b);
+    }
+    public double calcForceExertedByY(Body b)
+    {
+        double yGap = b.yyPos - this.yyPos;
+        return (this.calcForceExertedBy(b) * yGap)/this.calcDistance(b);
+    }
+    public double calcNetForceExertedByX(Body[] allBodys)
+    {
+        double netForceX = 0;
+        for (Body allBody : allBodys) {
+            if (!allBody.equals(this)) {
+                netForceX += this.calcForceExertedByX(allBody);
+            }
+        }
+        return  netForceX;
+    }
+    public double calcNetForceExertedByY(Body[] allBodys)
+    {
+        double netForceY = 0;
+        for (Body allBody : allBodys) {
+            if (!allBody.equals(this)) {
+                netForceY += this.calcForceExertedByY(allBody);
+            }
+        }
+        return  netForceY;
+    }
+    public void update(double dt, double fX, double fY)
+    {
+        /* fX fY are the forces add on the Body */
+        double aX = fX / this.mass;
+        double aY = fY / this.mass;
+        this.xxVel += aX * dt;
+        this.yyVel += aY * dt;
+        this.xxPos += this.xxVel * dt;
+        this.yyPos += this.yyVel * dt;
+    }
 }
