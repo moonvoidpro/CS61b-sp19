699aa363b60eaad6f142974a3f6a4e89ff6bd8ef
moonvoid <29794262+moonvoidpro@users.noreply.github.com>
2021-05-05 17:31:11 +0800

    finish basic Proj0

---
+++ proj0/BodyExtreme.java
@@ -0,0 +1,8 @@
+public class BodyExtreme {
+    //TODO: using StdAudio and the file 2001.mid
+    //TODO: Support elastic (or inelastic) collisions.
+    //TODO: Add the ability to programmatically generate
+    // planet images (rather than relying on input image files).
+    //TODO: Add the ability to control a spacecraft that is
+    // subject to the gravitational forces of the objects in the solar system.
+}
