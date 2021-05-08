e4b531d735d4fc6d0af202417d2c11b91922fc47
moonvoid <29794262+moonvoidpro@users.noreply.github.com>
2021-05-08 23:38:32 +0800

    deque not finished

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,50 @@
-public class ArrayDeque {
+public class ArrayDeque<T> {
+    private int size;
+    private T[] deque;
+    private int nextFirst;
+    private int nextLast;
+
+    //The starting size of your array should be 8.
+    public ArrayDeque(){
+        deque = (T[]) new Object[8];
+        size = 0;
+
+    }
+
+    public ArrayDeque(ArrayDeque other){
+
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T get (int index) {
+
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst() {
+
+    }
+
+    public void addLast() {
+
+    }
+
+    public T removeFirst() {
+
+    }
+
+    public T removeLast() {
+
+    }
+
+    public ArrayDeque(ArrayDeque other){
+
+    }
+
 }
